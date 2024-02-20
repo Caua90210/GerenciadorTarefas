29a0c36db55a3b416c92c5860ba709630ad549f2
Cauã <124824095+Caua90210@users.noreply.github.com>
2024-02-20 09:20:17 -0300

    Add files via upload

--- js/tarefas.js
+++ js/tarefas.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#app',
     data: {
       tasks: [
-        // Aqui você pode ter as tarefas iniciais carregadas do servidor
+       
       ],
       newTask: {
         title: '',
@@ -13,24 +13,24 @@ new Vue({
     },
     methods: {
       addTask() {
-        // Aqui você pode enviar a nova tarefa para o servidor e adicionar à lista
+        
         this.tasks.push({
           title: this.newTask.title,
           description: this.newTask.description,
           dueDate: this.newTask.dueDate
         });
-        // Limpa o formulário
+        
         this.newTask.title = '';
         this.newTask.description = '';
         this.newTask.dueDate = '';
         this.showForm = false;
       },
       editTask(index) {
-        // Aqui você pode implementar a lógica de edição da tarefa
+       
         console.log('Editar tarefa:', this.tasks[index]);
       },
       deleteTask(index) {
-        // Aqui você pode implementar a lógica para excluir a tarefa
+        
         this.tasks.splice(index, 1);
       }
     }
